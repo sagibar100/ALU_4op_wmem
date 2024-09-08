570e1611ed2d99887c99ce0b3da4ebd4c8e8358a
Reshef <reshef.schachter@s.afeka.ac.il>
2024-09-08 19:17:32 +0300

    (now with the actual file) TB prototype 2

--- tb_ALU.vhd
+++ tb_ALU.vhd
@@ -3,39 +3,42 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.numeric_std.all;
 
 entity tb_ALU is
-end tb_ALU;
+    Generic (
+        ADDR_WIDTH : integer := 2;
+        DATA_WIDTH : integer := 8
+    );
+end entity tb_ALU;
 
-architecture arc_tb_ALU of ALU is
-    constant DATA_WIDTH : integer := 8;
-    constant ADDR_WIDTH : integer := 2;
-    constant DATA_RANGE : range := DATA_WIDTH-1 downto 0;
-    constant ADDR_RANGE : range := ADDR_WIDTH-1 downto 0;
-    constant TEST_RANGE : range := 0 to 9;
+architecture arc_tb_ALU of tb_ALU is
+    -- constant DATA_WIDTH : integer := 8;
+    -- constant ADDR_WIDTH : integer := 2;
+    constant DATA       : std_logic_vector(DATA_WIDTH -1 downto 0) := (others => '0');
+    constant ADDRESS    : std_logic_vector(ADDR_WIDTH -1 downto 0) := (others => '0');
+    constant TEST       : std_logic_vector(0 to 9) := (others => '0');
     
-    type std_ctrl_array is array (natural range<>) of std_logic_vector(DATA_RANGE);
-    --aaray type with elements of std_logic_vector
-    type int_operands_array is array (natural range<>) of integer(2**(DATA_WIDTH-1) -1 downto -1 * 2**(DATA_WIDTH-1));
+    type std_ctrl_array is array (natural range<>) of std_logic_vector(DATA'range);
+    --array type with elements of std_logic_vector
+    type int_operands_array is array (natural range<>) of integer range 2**(DATA_WIDTH-1) -1 downto -1 * 2**(DATA_WIDTH-1);
     -- range is limited to integers represented by 8 bits in 2's complement
-    signal A  : int_operands_array(TEST_RANGE);
-    signal B  : int_operands_array(TEST_RANGE);
-    signal op : std_ctrl_array(TEST_RANGE);
+    signal A  : int_operands_array(TEST'range);
+    signal B  : int_operands_array(TEST'range);
+    signal op : std_ctrl_array(TEST'range);
     
-    alias zero  : std_logic_vector(DATA_RANGE) is "00000000";
-    alias zero  : std_logic_vector(DATA_RANGE) is "00000000";
-    alias sum   : std_logic_vector(DATA_RANGE) is "00000001";
-    alias sub   : std_logic_vector(DATA_RANGE) is "00000010";
-    alias mult  : std_logic_vector(DATA_RANGE) is "00000011";
-    alias div   : std_logic_vector(DATA_RANGE) is "00000100";
-    alias junk0 : std_logic_vector(DATA_RANGE) is "00000101";
-    alias junk1 : std_logic_vector(DATA_RANGE) is "00000110";
-    alias junk2 : std_logic_vector(DATA_RANGE) is "00000111";
-    alias junkA : std_logic_vector(DATA_RANGE) is "11111111";   
+    constant zero  : std_logic_vector(DATA'range) := "00000000";
+    constant sum   : std_logic_vector(DATA'range) := "00000001";
+    constant sub   : std_logic_vector(DATA'range) := "00000010";
+    constant mult  : std_logic_vector(DATA'range) := "00000011";
+    constant div   : std_logic_vector(DATA'range) := "00000100";
+    constant junk0 : std_logic_vector(DATA'range) := "00000101";
+    constant junk1 : std_logic_vector(DATA'range) := "00000110";
+    constant junk2 : std_logic_vector(DATA'range) := "00000111";
+    constant junkA : std_logic_vector(DATA'range) := "11111111";   
 
     signal clk,rst,rd_wr : std_logic := '1';
-    signal addr: std_logic_vector(ADDR_RANGE) :="00"
-    signal enable: std_logic := '0';
-    signal wr_data: std_logic_vector(DATA_RANGE) := (other => '0');
-    signal rd_data:std_logic_vector(DATA_RANGE);    
+    signal addr: std_logic_vector(ADDRESS'range) := "00";
+    signal en: std_logic := '0';
+    signal wr_data: std_logic_vector(DATA'range) := (others => '0');
+    signal rd_data:std_logic_vector(DATA'range);    
     signal res_out: STD_LOGIC_VECTOR((DATA_WIDTH*2)-1 downto 0);
     signal prev_res: STD_LOGIC_VECTOR((DATA_WIDTH*2)-1 downto 0); 
 
@@ -79,8 +82,12 @@ begin
         rd_wr   <= '0';
         wait until rising_edge(clk);
         
-        test_loop: for i in TEST_RANGE loop
-            prev_res <= res_out; 
+        test_loop: for i in TEST'range loop
+            prev_res <= res_out;
+            addr <= "11"; -- Address of execute
+            wr_data <= (0 => '0'); -- Deassert execute
+            wait until rising_edge(clk);
+             
             addr <= "00"; -- Address of A
             wr_data <= std_logic_vector(to_signed(A(i), DATA_WIDTH)); -- Convert integer to std_logic_vector
             wait until rising_edge(clk);
@@ -91,37 +98,34 @@ begin
             wr_data <= op(i);
             wait until rising_edge(clk);
             addr <= "11"; -- Address of execute
-            wr_data <= (0 => '1', others => '0');
+            wr_data <= (0 => '1'); -- Execute
             wait until rising_edge(clk);
             wait until rising_edge(clk);
-
-            -- Check the result
-            case op(i) is
-                when zero =>
-                    assert res_out = std_logic_vector(to_signed(0, DATA_WIDTH*2))
-                        report "Zero operation failed!" severity warning;
-                when sum =>
-                    assert res_out = std_logic_vector(to_signed(A(i) + B(i), DATA_WIDTH*2))
-                        report "Sum operation failed!" severity warning;
-                when sub =>
-                    assert res_out = std_logic_vector(to_signed(A(i) - B(i), DATA_WIDTH*2))
-                        report "Sub operation failed!" severity warning;
-                when mult =>
-                    assert res_out = std_logic_vector(to_signed(A(i) * B(i), DATA_WIDTH*2))
-                        report "Mult operation failed!" severity warning;
-                when div =>
-                    if (B(i) = 0) then
-                        assert res_out = x"DEAD";
-                            report "Div operation failed!" severity warning;
-                    else
-                        assert res_out = std_logic_vector(to_signed(A(i) / B(i), DATA_WIDTH*2))
-                            report "Div operation failed!" severity warning;
-                    end if;
-                when others =>
-                    assert res_out = prev_res
-                            report "OTHERS operation failed!" severity warning;
-                     
-            end case;
+            
+            if(op(i) = zero) then
+                assert res_out = std_logic_vector(to_signed(0, DATA_WIDTH*2))
+                    report "Zero operation failed!" severity warning;
+            elsif(op(i) = sum) then
+                assert res_out = std_logic_vector(to_signed(A(i) + B(i), DATA_WIDTH*2))
+                    report "Sum operation failed!" severity warning;
+            elsif(op(i) = sub) then
+                assert res_out = std_logic_vector(to_signed(A(i) - B(i), DATA_WIDTH*2))
+                    report "Sub operation failed!" severity warning;
+            elsif(op(i) = mult) then
+                assert res_out = std_logic_vector(to_signed(A(i) * B(i), DATA_WIDTH*2))
+                    report "Mult operation failed!" severity warning;
+            elsif(op(i) = div) then
+                if(B(i) = 0) then
+                    assert res_out = x"DEAD";
+                        report "Div operation failed!" severity warning;
+                else
+                    assert res_out = std_logic_vector(to_signed(A(i) / B(i), DATA_WIDTH*2))
+                        report "Div operation failed!" severity warning;
+                end if;
+            else
+                assert res_out = prev_res
+                        report "OTHERS operation failed!" severity warning;
+            end if;
 
         end loop test_loop;
 
